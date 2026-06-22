83dfd2f62fb5cac2760e71ceccf757113d46116b
vaibhawxyz <vaibhawxyz@gmail.com>
2026-06-22 22:56:40 +0530

    prototype chaining and obj method

--- 01-js-internals/prototype/inheritance-patterns.js
+++
@@ -1,7 +0,0 @@
-// inheritance-patterns.js
-// Classical (class/extends) vs prototypal inheritance — same model, two styles.
-//
-// TODO:
-//  - implement a small hierarchy with `class ... extends`
-//  - re-implement it with constructor functions + Object.create
-//  - note what `super` desugars to
--- 01-js-internals/prototype/prototype-chain.js
+++ 01-js-internals/prototype/prototype-chain.js
@@ -5,3 +5,18 @@
 //  - build objects with Object.create(proto) and trace lookups up the chain
 //  - compare obj.__proto__ vs Object.getPrototypeOf(obj)
 //  - show where own properties shadow inherited ones
+let animal = {
+    alive: true
+}
+
+let cat = {
+    isAnimal: true,
+    color: 'black'
+}
+let dog = Object.create(animal) // this will create new obj with exiting properites of arg obj
+console.log(dog.alive)
+Object.setPrototypeOf(cat, animal) // new way to set __proto__ : earlier we do this `cat.__proto__ = animal`
+
+console.log(animal.alive)
+console.log(cat.alive)
+
