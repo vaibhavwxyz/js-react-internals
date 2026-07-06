cfbfe535e59ab4de1a3f3508ab0dfe36bd0b3cd9
vaibhawxyz <vaibhawxyz@gmail.com>
2026-07-06 22:57:24 +0530

    test

--- 02-browser-internals/web-workers/main.js
+++
@@ -1,7 +0,0 @@
-// main.js
-// Spawns worker.js, exchanges messages, demonstrates Transferable objects.
-//
-// TODO:
-//  - new Worker('./worker.js', { type: 'module' })
-//  - postMessage a task; handle the reply
-//  - transfer an ArrayBuffer and observe it becoming detached on this side
