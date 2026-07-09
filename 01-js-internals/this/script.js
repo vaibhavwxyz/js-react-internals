ed3108d52ea1514221a204a8b9831c5257c6cb45
vaibhawxyz <vaibhawxyz@gmail.com>
2026-07-10 00:02:10 +0530

    this

--- 01-js-internals/this/script.js
+++ 01-js-internals/this/script.js
@@ -7,10 +7,25 @@ function whoIam() {
 const obj1 = {
   name: "vaibhav",
   hi: function () {
-    console.log(this.name);
+    console.log("this is obj1", this);
+  },
+};
+
+const obj2 = {
+  a: 10,
+  x: () => {
+    console.log("this is obj2", this);
+  },
+  z: function () {
+    let y = () => {
+      console.log("this is arrow z", this);
+    };
+    y();
   },
 };
 
 obj1.hi();
+obj2.x();
+obj2.z();
 
-whoIam();
+// whoIam();
