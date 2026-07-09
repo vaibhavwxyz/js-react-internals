05d525d67d477f88ac843c657d1d80b1bbd26c1a
vaibhawxyz <vaibhawxyz@gmail.com>
2026-07-09 23:55:50 +0530

    this

--- 01-js-internals/this/script.js
+++ 01-js-internals/this/script.js
@@ -3,3 +3,14 @@ console.log(this);
 function whoIam() {
   console.log("inside fun", this);
 }
+
+const obj1 = {
+  name: "vaibhav",
+  hi: function () {
+    console.log(this.name);
+  },
+};
+
+obj1.hi();
+
+whoIam();
