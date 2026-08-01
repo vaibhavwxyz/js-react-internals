589e87a8df91b01b249d2bd8cdc7640b6f9dabb2
vaibhawxyz <vaibhawxyz@gmail.com>
2026-08-01 23:24:14 +0530

    set

--- 01-js-internals/polyfill/script.js
+++ 01-js-internals/polyfill/script.js
@@ -2,3 +2,16 @@ let arr = [2, 8, 9, 1, 8, 2, 4, 7];
 let ans = new Set(arr);
 let at = ans;
 console.log(at);
+
+// --- Basic operations ---
+ans.add(10); // add a value
+ans.delete(9); // remove a value
+console.log(ans.has(8)); // check membership -> true
+console.log(ans.size); // number of elements
+console.log([...ans]); // convert back to array
+
+// --- Iterating ---
+for (let val of ans) {
+  console.log(val);
+}
+ans.forEach((val) => console.log(val));
